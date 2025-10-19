c0a0f854884f4f6452e8d37461875aa1963a08dc
m0704a <m0704a@gmail.com>
2025-10-19 15:20:00 +0000

    Add account

--- src/main/java/org/muge/bankomat/account/AccountController.java
+++ src/main/java/org/muge/bankomat/account/AccountController.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AccountController {
@@ -21,5 +23,23 @@ public class AccountController {
         model.addAttribute("accounts", accountRepository.findByCompanyId(companyId));
         companyRepository.findById(companyId).ifPresent(company -> model.addAttribute("company", company));
         return "account/account-list";
+    }
+
+    @GetMapping("/company/{companyId}/add-account")
+    public String showAddAccountForm(@PathVariable Long companyId, Model model) {
+        companyRepository.findById(companyId).ifPresent(company -> {
+            model.addAttribute("company", company);
+            Account account = new Account();
+            account.setCompany(company);
+            model.addAttribute("account", account);
+        });
+        return "account/add-account";
+    }
+
+    @PostMapping("/company/{companyId}/add-account")
+    public String addAccount(@PathVariable Long companyId, @ModelAttribute Account account) {
+        companyRepository.findById(companyId).ifPresent(account::setCompany);
+        accountRepository.save(account);
+        return "redirect:/company/" + companyId + "/accounts";
     }
 }
